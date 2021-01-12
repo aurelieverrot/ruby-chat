f7f67a768551398daa2edf4061607f1e16d9a1a4
Aurelie Verrot <aurelie+perangusta@verrot.fr>
2021-01-11 19:53:36 -0800

    created a chat in command line with Ruby

---
+++ client.rb
@@ -0,0 +1,30 @@
+require 'socket'
+
+name = ARGV.shift
+s = TCPSocket.new 'localhost', 2000
+
+s.puts "#{name}"
+
+def local_typing socket
+  loop do
+    print "(me)> "
+    text_to_send = gets.chomp
+    socket.puts text_to_send
+    # puts "\"#{text_to_send}\" sent"
+  end
+end
+
+def receive_from_server socket
+  while line = socket.gets # Read lines from socket
+    puts line         # and print them
+  end
+end
+
+
+lt = Thread.new { local_typing(s) }
+rfs = Thread.new { receive_from_server(s) }
+
+lt.join
+rfs.join
+
+s.close             # close socket when done
